4a78e20c108533b50cf1e1b9b832e3d503fc1406
Mike <20vampir11@gmail.com>
2021-12-19 16:02:57 +0300

    Мделали, чтобы все двигалось!!!!

--- CarRacing/Form1.cs
+++ CarRacing/Form1.cs
@@ -12,6 +12,11 @@ namespace CarRacing
 {
     public partial class Form1 : Form
     {
+        int timerSpeed;
+        int gameSpeed;
+        int carTurnSpeed;
+        bool isGameOver;
+
         PictureBox[] roadlines;
         PictureBox[] enemys;
         PictureBox[] coins;
@@ -25,51 +30,111 @@ namespace CarRacing
 
         void init_game()
         {
-            
+            timerSpeed = 100;
+            gameSpeed = 4;
+            carTurnSpeed = 15;
+            isGameOver = false;
+
+            roadlines = new PictureBox[] { pbRoadLine1, pbRoadLine2, pbRoadLine3, pbRoadLine4 };
+            enemys = new PictureBox[] { Enemy1, Enemy2, Enemy3 };
+            coins = new PictureBox[] { Coin1, Coin2, Coin3, Coin4 };
+            random = new Random();
+            collectedCoins = 0;
+
+            gen_start_pos();
+
+            Car.Image = Properties.Resources.car;
+            lblGameOver.Visible = false;
+
+            gameTimer.Interval = 1000 / timerSpeed;
+            gameTimer.Start();
         }
 
         void gen_start_pos()
         {
-            
+            int startY = -Enemy1.Height;
+            int x;
+            for(int i = 0; i < enemys.Length; i++)
+            {
+                x = random.Next(pbBoundLeft.Right, pbBoundRight.Left);
+                enemys[i].Location = new Point(x, startY);
+                startY -= this.Height / enemys.Length;
+            }
         }
 
         void gameover_actions()
         {
-            
+
         }
 
-        void is_gameover() 
-        { 
-            
+        void is_gameover()
+        {
+
         }
 
         void is_get_coins()
         {
-            
+
         }
 
         void move_enemys()
         {
-            
+            int x; 
+            for (int i = 0; i < enemys.Length; i++)
+            {
+                if(enemys[i].Top > this.Height)
+                {
+                    x = random.Next(pbBoundLeft.Right, pbBoundRight.Left - enemys[i].Width);
+                    enemys[i].Location = new Point(x, -enemys[i].Height);
+                }
+                else
+                {
+                    enemys[i].Top += gameSpeed;
+                }
+            }
         }
         void move_coins()
         {
-            
+            int x;
+            for(int i =0; i < coins.Length; i++)
+            {
+                if(coins[i].Top > this.Height)
+                {
+                    x = random.Next(pbBoundLeft.Right, pbBoundRight.Left - coins[i].Width);
+                    coins[i].Location = new Point(x, -coins[i].Height);
+                }
+                else
+                {
+                    coins[i].Top += gameSpeed;
+                }
+            }
         }
 
-        void move_lines() 
+        void move_lines()
         {
-            
+            for( int i = 0; i< roadlines.Length; i++)
+            {
+                if(roadlines[i].Top > this.Height)
+                {
+                    roadlines[i].Top = -roadlines[i].Height;
+                }
+                else
+                {
+                    roadlines[i].Top += gameSpeed;
+                }
+            }
         }
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
-            
+            move_enemys();
+            move_coins();
+            move_lines();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            
+
         }
     }
 }
